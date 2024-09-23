c280da0174e68a68fecaffd6bd36005621d4a9c6
Khirushan-Sasitharan-ICD2O1-01 <khirushan.sasitharan@student.tdsb.on.ca>
2024-09-23 14:20:53 +0000

    ddd

--- PP1_5.py
+++ PP1_5.py
@@ -4,35 +4,36 @@ def q1():
   what = int(skbidi)
   dddd = what + 3 
   word = f"Your number is now {dddd}"
-  print (word)
+  print (dddd)
 
 def q2():
     skddi = input("Input a number: ")
-    skddid = skddi + 4
-    hi = float(skddid)
-    him = hi + 2
-    print (f"Your number is now {him}") 
+    skddid = str(skddi) + "4"
+    him = float(skddid) + 2
+    print (him)
 def q3():
     ohioi = input("Input a radius: ")
     hey = float(ohioi)
     poopy = hey*hey
-    print (f"Your number is now {poopy*3.14}") 
+    dads = (f"{poopy*3.14}") 
+    print (dads)
 def q4():
     ooi = input("Input a number: ")
     hedy = float(ooi)
     podopy = hedy*12
-    print (f"Your number is now {podopy//1}") 
+    aaaa = (f"{int(podopy)}") 
+    print (aaaa)
 def q5():
   kbidi = input("Input an integer: ")
   whadt = int(kbidi)
   wut = whadt + 5
-  print (f"Your number + 5 is {wut + 3}")
+  print (f"Your number + 5 is {wut}")
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
